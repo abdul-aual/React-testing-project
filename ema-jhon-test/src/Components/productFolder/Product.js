4e3e8787b3345ad1796f33b84e7a4a2ee4d5e1e6
abdul-aual <mdabdulaual38@gmail.com>
2024-07-08 00:03:08 +0600

    hello

--- ema-jhon-test/src/Components/productFolder/Product.js
+++ ema-jhon-test/src/Components/productFolder/Product.js
@@ -1,22 +1,47 @@
+// import React from 'react';
+// import './product.css';
+// import { Link } from 'react-router-dom';
+
+// const Product = (props) => {
+//     const {name, img, seller, price, stock,key}= props.productDetails;
+//     return (
+//         <div className='single-one-product'>
+//             <div className='product-image'>
+//                 <img src={img} alt="" />
+//             </div>
+//             <div className='product-name' > 
+//                 <h4><Link className='name' to={'/product/'+key}>{name}</Link></h4><br />
+//                 <p><small>seller: {seller}</small></p>
+//                 <p style={{fontWeight:'bold'}}>Price: ${price}</p>
+//                 <p><small>Only {stock} left in stock - Order soon</small></p>
+//                 <button className='btn-mama' onClick={()=>props.handleAddCart(props.productDetails)} >add to cart</button>
+//             </div>
+            
+//         </div>
+//     );
+// };
+
+// export default Product;
+
 import React from 'react';
 import './product.css';
 import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    const {name, img, seller, price, stock,key}= props.productDetails;
+    const { name, img, seller, price, stock, key } = props.productDetails;
+    
     return (
         <div className='single-one-product'>
             <div className='product-image'>
                 <img src={img} alt="" />
             </div>
-            <div className='product-name' > 
-                <h4><Link className='name' to={'/product/'+key}>{name}</Link></h4><br />
-                <p><small>seller: {seller}</small></p>
-                <p style={{fontWeight:'bold'}}>Price: ${price}</p>
+            <div className='product-name'>
+                <h4><Link className='name' to={'/product/' + key}>{name}</Link></h4><br />
+                <p><small>Seller: {seller}</small></p>
+                <p style={{fontWeight: 'bold'}}>Price: ${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className='btn-mama' onClick={()=>props.handleAddCart(props.productDetails)} >add to cart</button>
+                <button className='btn-mama' onClick={() => props.handleAddCart(props.productDetails)}>Add to Cart</button>
             </div>
-            
         </div>
     );
 };
