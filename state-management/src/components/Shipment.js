18be0cd77aab54f23281c7597f80871bba7cbe01
abdul-aual <mdabdulaual38@gmail.com>
2024-07-28 20:36:26 +0600

    hello

---
+++ state-management/src/components/Shipment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shipment = () => {
+    return (
+        <div>
+            <p>This is shipment section</p>
+        </div>
+    );
+};
+
+export default Shipment;
