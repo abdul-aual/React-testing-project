f1e2703a0ba3800ce7faf312f2ad1e6090f4630c
abdul-aual <mdabdulaual38@gmail.com>
2024-09-24 17:14:55 +0600

    hello world

--- ema-jhon-test/src/App.js
+++ ema-jhon-test/src/App.js
@@ -1,30 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './Components/headerFolder/Header';
-import Shop from './Components/shopFolder/Shop';
-import Inventory from './Components/inventoryFolder/Inventory';
-import Review from './Components/reviewFolder/Review';
-import NotFound from './Components/notFound/NotFound';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import PdDetails from './Components/pdDetailsFolder/PdDetails';
+
 function App() {
   return (
-    <div>
-      <Router>
-      <Header></Header>
-        <Routes>
-          <Route path='/shop' element={<Shop></Shop>} />
-          <Route path='/review' element={<Review></Review>} />
-          <Route path='/inventory' element={<Inventory></Inventory>} />
-          <Route exact path='/' element={<Shop></Shop>}></Route>
-          <Route path='/product/:productKey' element={<PdDetails></PdDetails>}></Route>
-          <Route path='*' element={<NotFound></NotFound>}></Route>
-
-        </Routes>
-      </Router>    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
